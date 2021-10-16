9e99312767274ad8ecefd23a69bfdab68be39d84
Carla Carvalho <carla-carvalho@outlook.com>
2021-10-16 02:08:28 -0300

    commit

--- index.js
+++ index.js
@@ -1,16 +1,52 @@
-require('dotenv').config()
 const express = require("express");
 const path = require("path");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+const desapega = [];
+let message = "";
 
-app.set("view engine", "ejs");
+app.use(express.urlencoded());
 app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.render("index");
+  setTimeout(() => {
+    message = "";
+  }, 1000);
+
+  res.render("index", {
+    desapega,
+  });
 });
+
+app.get("/cadastro", (req, res) => {
+  res.render("cadastro");
+});
+
+app.get("/catalogo", (req, res) => {
+res.render("catalogo", {
+  desapega,
+  message
+  });
+});
+
+app.post("/new", (req, res) => {
+  const produto = req.body;
+  desapega.push(produto);
+  message = "Item cadastrado com sucesso!";
+  res.redirect("/catalogo");
+});
+
+app.get("/detalhes/:id", (req, res) => {
+  const id = req.params.id;
+  const produto = desapega[id];
+  res.render("detalhes", {
+    produto,
+  });
+});
+
+
 
 app.listen(port, () =>
   console.log(`Servidor rodando em http://localhost:${port}`)
